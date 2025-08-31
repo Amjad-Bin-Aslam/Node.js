b0caa83580d22231fbf48ca138ff0c6095e3641a
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-08-31 23:57:38 +0500

    added

--- 02-node/index.js
+++ 02-node/index.js
@@ -1,13 +1,25 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url");
+// const { url } = require("inspector");
+
 const myServer = http.createServer((req , res) => {
+    if (req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}:${req.url} New Req. Received\n`
+    const myUrl = url.parse(req.url , true)
+    console.log(myUrl)
     fs.appendFile("log.txt" , log , (err , data) => {
-    switch(req.url){
+    switch(myUrl.pathname){
         case '/': res.end("HomePage")
         break;
-        case "/about": res.end("About Page")
-        break
+        case "/about": 
+        const username = myUrl.query.myName
+        res.end(`Hi, ${username}`)
+        break;
+        case "/search":
+            const search = myUrl.query.search_query;
+            res.end(`${search}`)
+            break;
         default:
             res.end("404 Not found")
     }
