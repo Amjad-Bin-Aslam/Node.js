39cb940841124a67d01bb2e24b6a17607a417ae0
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-01 14:58:34 +0500

    added

--- 02-node/index.js
+++ 02-node/index.js
@@ -1,30 +1,19 @@
 const http = require("http")
-const fs = require("fs")
-const url = require("url");
-// const { url } = require("inspector");
+const express = require("express")
 
-const myServer = http.createServer((req , res) => {
-    if (req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()}:${req.url} New Req. Received\n`
-    const myUrl = url.parse(req.url , true)
-    console.log(myUrl)
-    fs.appendFile("log.txt" , log , (err , data) => {
-    switch(myUrl.pathname){
-        case '/': res.end("HomePage")
-        break;
-        case "/about": 
-        const username = myUrl.query.myName
-        res.end(`Hi, ${username}`)
-        break;
-        case "/search":
-            const search = myUrl.query.search_query;
-            res.end(`${search}`)
-            break;
-        default:
-            res.end("404 Not found")
-    }
-    })
-});
+const app = express();
+
+app.get("/" , (req , res) => {
+    return res.send("Hello from homepage")
+})
+
+app.get("/about", (req , res) => {
+    return res.end("Hello from about page")
+})
+
+
+
+const myServer = http.createServer(app);
 
 
 myServer.listen(8000 , () => console.log("server started"));
