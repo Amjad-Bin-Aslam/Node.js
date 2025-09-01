8e442646eb09db6701909d2aa846bf1e9eecb581
Amjad-Bin-Aslam <amjadbinaslam604@gmail.com>
2025-09-01 16:00:19 +0500

    added

--- 02-node/index.js
+++ 02-node/index.js
@@ -1,19 +1,37 @@
 const http = require("http")
 const express = require("express")
+const fs = require("fs");
+
 
 const app = express();
 
 app.get("/" , (req , res) => {
-    return res.send("Hello from homepage")
-})
+    const logMessage = "Hello from HomePage\n"
+    res.send(logMessage)
+
+    fs.appendFile("./log.txt" , logMessage , (err) => {
+    if(err){
+        console.log("ERROR: " , err)
+    } else{
+        console.log("success")
+    }
+});
+});
+
+
 
 app.get("/about", (req , res) => {
-    return res.end("Hello from about page")
+    const logMessage = "Hello From About page\n"
+    res.end(`${logMessage }${req.query.name}`)
+    fs.appendFile("./log.txt" , logMessage , (err) => {
+    if(err){
+        console.log("ERROR: " , err)
+    }else{
+        console.log("About")
+    }
+})
 })
 
 
 
-const myServer = http.createServer(app);
-
-
-myServer.listen(8000 , () => console.log("server started"));
+app.listen(8000 , () => console.log("server started"));
